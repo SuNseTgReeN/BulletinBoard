ce20836a3d245808e1ee7d4e5cb1696bcce68b05
SuNseTgReeN <ahtohio_pal09@bk.ru>
2023-03-21 21:22:28 +0200

    Добавил умную строку поиска. Поиск проходит по полям title и text

--- BulletinBoardProject/BulletinBoard/Board/filters.py
+++ BulletinBoardProject/BulletinBoard/Board/filters.py
@@ -1,11 +1,18 @@
-from django_filters import FilterSet, filters
+import django_filters
+from django_filters import FilterSet
 from .models import Notification
+from django.db.models import Q
 
 
 class ProductFilter(FilterSet):
+    q = django_filters.CharFilter(method='my_custom_filter', label="search")
+
     class Meta:
         model = Notification
-        fields = {
-            'title': ['icontains'],
-            # 'text': ['icontains'],
-        }
+        fields = ['q']
+
+    def my_custom_filter(self, queryset, name, value):
+        return queryset.filter(
+            Q(title__icontains=value) |
+            Q(text__icontains=value)
+        )
